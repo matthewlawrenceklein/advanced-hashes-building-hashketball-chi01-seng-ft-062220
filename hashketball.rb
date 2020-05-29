a3b987fa344191000f22e6af082b4927a65a3ab3
Matthew Klein <mtt.l.kln@gmail.com>
2020-05-29 20:13:15 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -64,47 +64,67 @@ the_game = {
     ],
   }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  # :away
-  # {
-  #   :team_name => [],
-  #   :colors => [],
-  #   :players => [
-  #     :player_name =>,
-  #     :number =>,
-  #     :shoe =>,
-  #     :points =>,
-  #     :rebounds =,
-  #     :assists =>,
-  #     :steals =>,
-  #     :blocks =>,
-  #     :slam_dunks =>,
-  #   ],
-  # }
+  :away
+  {
+    :team_name => [],
+    :colors => [],
+    :players => [ {
+      :player_name =>,
+      :number =>,
+      :shoe =>,
+      :points =>,
+      :rebounds =>,
+      :assists =>,
+      :steals =>,
+      :blocks =>,
+      :slam_dunks =>,
+    },
+    {
+      :player_name =>,
+      :number =>,
+      :shoe =>,
+      :points =>,
+      :rebounds =>,
+      :assists =>,
+      :steals =>,
+      :blocks =>,
+      :slam_dunks =>,
+    },
+    {
+      :player_name =>,
+      :number =>,
+      :shoe =>,
+      :points =>,
+      :rebounds =>,
+      :assists =>,
+      :steals =>,
+      :blocks =>,
+      :slam_dunks =>,
+    },
+    {
+      :player_name =>,
+      :number =>,
+      :shoe =>,
+      :points =>,
+      :rebounds =>,
+      :assists =>,
+      :steals =>,
+      :blocks =>,
+      :slam_dunks =>,
+    },
+    {
+      :player_name =>,
+      :number =>,
+      :shoe =>,
+      :points =>,
+      :rebounds =>,
+      :assists =>,
+      :steals =>,
+      :blocks =>,
+      :slam_dunks =>,
+    }
+    ],
+  }
 }
 
 return the_game
