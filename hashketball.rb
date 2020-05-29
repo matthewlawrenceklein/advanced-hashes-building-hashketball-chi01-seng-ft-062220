6e767e4269bf569c67d21be96a370f1c7de7f3dd
Matthew Klein <mtt.l.kln@gmail.com>
2020-05-29 20:08:21 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,112 @@
 # Write your code here!
+def game_hash
+
+the_game = {
+
+  :home => {
+    :team_name => ["Brooklyn Nets"],
+    :colors => ["Black", "White"],
+    :players => [ {
+      :player_name => "Alan Anderson",
+      :number => 0,
+      :shoe => 16,
+      :points => 22,
+      :rebounds = 12,
+      :assists => 12,
+      :steals => 3,
+      :blocks => 1,
+      :slam_dunks => 1, 
+    },
+    {
+      :player_name => "Reggie Evans",
+      :number => 30,
+      :shoe => 14,
+      :points => 12,
+      :rebounds = 12,
+      :assists => 12,
+      :steals => 12,
+      :blocks => 12,
+      :slam_dunks => 7, 
+    },
+    {
+      :player_name => "",
+      :number =>,
+      :shoe =>,
+      :points =>,
+      :rebounds =,
+      :assists =>,
+      :steals =>,
+      :blocks =>,
+      :slam_dunks =>, 
+    },
+    {
+      :player_name => "",
+      :number =>,
+      :shoe =>,
+      :points =>,
+      :rebounds =,
+      :assists =>,
+      :steals =>,
+      :blocks =>,
+      :slam_dunks =>, 
+    },
+    {
+      :player_name => "",
+      :number =>,
+      :shoe =>,
+      :points =>,
+      :rebounds =,
+      :assists =>,
+      :steals =>,
+      :blocks =>,
+      :slam_dunks =>, 
+    },
+    ],
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+  :away
+  {
+    :team_name => [],
+    :colors => [],
+    :players => [
+      :player_name =>,
+      :number =>,
+      :shoe =>,
+      :points =>,
+      :rebounds =,
+      :assists =>,
+      :steals =>,
+      :blocks =>,
+      :slam_dunks =>, 
+    ],
+  }  
+}
+
+return the_game
+
+end
